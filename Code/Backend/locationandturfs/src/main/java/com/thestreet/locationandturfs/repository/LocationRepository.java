6a5cbbf5e84121c4e6729342aa792d41c3b17079
Akhil Aduvala <aduvalaakhil@gmail.com>
2024-06-02 00:55:42 +0530

    project corrections

--- Code/Backend/locationandturfs/src/main/java/com/thestreet/locationandturfs/repository/LocationRepository.java
+++ Code/Backend/locationandturfs/src/main/java/com/thestreet/locationandturfs/repository/LocationRepository.java
@@ -6,7 +6,6 @@ package com.thestreet.locationandturfs.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.thestreet.common.dto.LocationsDTO;
 import com.thestreet.locationandturfs.model.Location;
 
 /**
---
+++ Code/Backend/master/src/main/java/com/thestreet/tbms/MasterApplication.java
@@ -0,0 +1,16 @@
+package com.thestreet.tbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MasterApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(MasterApplication.class, args);
+
+	}
+
+}
