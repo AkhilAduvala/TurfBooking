be5b909ee048ac78e1476971c0c400d60fe21363
Akhil Aduvala <aduvalaakhil@gmail.com>
2024-06-03 00:05:06 +0530

    configuring the application.yml and log4j2 for db connection and logging

--- Code/Backend/locationandturfs/src/main/java/com/thestreet/locationandturfs/controller/LocationAndTurfs.java
+++ Code/Backend/locationandturfs/src/main/java/com/thestreet/locationandturfs/controller/LocationAndTurfs.java
@@ -5,6 +5,8 @@ package com.thestreet.locationandturfs.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +29,8 @@ public class LocationAndTurfs {
 	@Autowired
 	private LocationAndTurfsService locationAndTurfsService;
 	
+	private static final Logger logger = LogManager.getLogger(LocationAndTurfs.class);
+		
 	/**
 	 * 
 	 * getLocations() is to fetch all the available locations
@@ -36,6 +40,7 @@ public class LocationAndTurfs {
 	 * */
 	@GetMapping("/get-location")
 	public ResponseEntity<List<Location>> getLocations(){
+		logger.info("Entering the get-location method");
 		List<Location> locations = locationAndTurfsService.getLocations();
 		return ResponseEntity.ok(locations);
 	}
