d70c8f2c1f83c65e0b8f76eadd6e0b754c245a21
Akhil Aduvala <aduvalaakhil@gmail.com>
2024-06-14 15:10:06 +0530

    adding main class for authentication

---
+++ Code/Backend/authentication/src/main/java/com/thestreet/authentication/main/AuthenticationApplication.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.thestreet.authentication.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ */
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(AuthenticationApplication.class, args);
+
+	}
+
+}
---
+++ Code/Backend/authentication/src/main/java/com/thestreet/authentication/main/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package com.thestreet.authentication.main;
